0970583f5a775fb69e779138a57aead4e118c7f5
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-02-28 13:54:46 -0500

    Completed challenge to guess the number using js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-alert('Hey find me to know where to put the javascript');
+var name = prompt("What's your name?")
+var guessMe = prompt("Hi " + name + "! Guess a number between 0-100!");
+var randomNumber = Math.round(Math.random()*101);
+if (randomNumber === parseFloat(guessMe)) {
+  alert(guessMe + " is the correct number! You win!  Yay!");
+} else {
+  alert(guessMe + " is the wrong number! You lose.")
+}
+
+
+
+//alert('Hey find me to know where to put the javascript');
